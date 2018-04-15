9f2b7e1dfd9b7c8d7eddbfd8f25d32992dbdb480
Leslie Stevens <flylady@mail.utexas.edu>
2018-04-15 14:27:05 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+
+end
+
+def batch_badge_creator(names)
+  names.collect{|name| badge_maker(name)}
+end
+
+def assign_rooms(names)
+  names.collect.each_with_index {|name, index|
+    "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+end
+
+def printer
+  puts batch_badge_creator
+end
+    
+
+
