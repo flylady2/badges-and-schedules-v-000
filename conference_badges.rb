791f53d0954ade05918544b79762388f8916acb7
Leslie Stevens <flylady@mail.utexas.edu>
2018-04-15 15:20:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,10 +18,10 @@ def printer(attendees)
   batch_badge_creator(attendees).each do|badge|
     puts badge
   end
-    assign_rooms(attendees).each do|assignment|
-      puts assignment
-    end
+  assign_rooms(attendees).each do|assignment|
+    puts assignment
   end
+
 
 
 
